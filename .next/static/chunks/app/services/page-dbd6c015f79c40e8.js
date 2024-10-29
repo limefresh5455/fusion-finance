fdb0eeb2924a308100a6c8f827e86e4c4732a24c
limefresh5455 <71378854+limefresh5455@users.noreply.github.com>
2024-10-29 17:48:23 +0530

    Delete .next directory

--- .next/static/chunks/app/services/page-dbd6c015f79c40e8.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[469],{3740:function(e,n,t){Promise.resolve().then(t.t.bind(t,1749,23)),Promise.resolve().then(t.t.bind(t,5250,23)),Promise.resolve().then(t.t.bind(t,9978,23))},9978:function(){}},function(e){e.O(0,[749,250,971,69,744],function(){return e(e.s=3740)}),_N_E=e.O()}]);
