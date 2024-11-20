9a20a0b53c7b64cc0b2fd1d52325740a226f4f49
nileshlin <nileshlinux01@gmail.com>
2024-11-20 17:51:50 +0530

    add email

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -13,7 +13,7 @@ export default function sendEmail(req, res) {
 
   const mailData = {
     from: "allison@branue.com",
-    to: "info@fusion-finance.com",
+    to: "gemma@fusion-finance.com", //for the testing purpose i am enter my email info@fusion-finance.com gemma@fusion-finance.com
     subject: "Email From Fusion Finance",
     html: `
       <div>
